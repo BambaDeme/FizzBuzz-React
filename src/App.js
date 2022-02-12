db5da3d30f7c2b68faf14cf11cd24ffdc2320b66
bambademe <cheikhbambademe@gmail.com>
2022-02-11 18:39:00 -0800

    components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
+import React, { useState } from "react";
 import "./App.css";
+import { fizzbuzz } from "./components/fizzBuzz";
+import Prev from "./components/Prev";
+import Reset from "./components/Reset";
+import Next from "./components/Next";
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(1);
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="">
         <h1>Fizz Buzz</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
       </header>
 
-      <FizzBuzz counter={counter} setCounter={setCounter} />
+      <section className="counter">{fizzbuzz(counter)}</section>
+
+      <div className="actions">
+        <Prev counter={counter} setCounter={setCounter} />
+        <Reset counter={counter} setCounter={setCounter} />
+        <Next counter={counter} setCounter={setCounter} />
+      </div>
     </div>
   );
 }
